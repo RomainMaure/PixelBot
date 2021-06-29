1aed866445960462916cfa963a1996d96332df53
romain <romain.maure@epfl.ch>
2021-06-29 23:26:01 +0200

    blinking sequence

--- src/main.py
+++ src/main.py
@@ -3,34 +3,72 @@
 import pygame
 from pygame.locals import *
 
-############################### GLOBAL VARIABLES ##############################
+################################## VARIABLES ##################################
 
-continuer = True
+# Eye blinking
+blink = []
+is_blinking = False
+count_blinking = 0
+count_blinking_state = 0
+blinking_state = 0
+
+BLINKING_TIME = 400
+CHANGE_BLINK_STATE = 5
+
+
+# General use
+animation = True
+
 FPS = 100
 DIMENSIONS = (800, 480)
-WHITE = (255, 255, 255)
 
 ################################## MAIN #######################################
 
 pygame.init()
 
-fenetre = pygame.display.set_mode(DIMENSIONS)
+window = pygame.display.set_mode(DIMENSIONS)
 pygame.display.set_caption("PixelBot")
 
-fond = pygame.Surface(DIMENSIONS)
-fond.fill(WHITE)
+blink.append(pygame.image.load("../img/blink/blink_1.png").convert_alpha())
+blink.append(pygame.image.load("../img/blink/blink_2.png").convert_alpha())
+blink.append(pygame.image.load("../img/blink/blink_3.png").convert_alpha())
+blink.append(pygame.image.load("../img/blink/blink_4.png").convert_alpha())
+blink.append(pygame.image.load("../img/blink/blink_5.png").convert_alpha())
 
-fenetre.blit(fond, (0, 0))
+blink = [pygame.transform.scale(img, DIMENSIONS) for img in blink]
+blink += reversed(blink)
+blink.remove(blink[len(blink)//2])
+
 
 clock = pygame.time.Clock()
 
-while continuer: 
+while animation: 
 
     clock.tick(FPS)
 
     for event in pygame.event.get():
         if event.type == QUIT:
-            continuer = False
+            animation = False
+
+    # Blinking
+    count_blinking += 1
+
+    if count_blinking == BLINKING_TIME:
+        is_blinking = True
+
+    if is_blinking:
+        count_blinking_state += 1
+
+        if count_blinking_state == CHANGE_BLINK_STATE:
+            blinking_state += 1
+            count_blinking_state = 0
+
+            if blinking_state == len(blink):
+                count_blinking = 0
+                blinking_state = 0
+                is_blinking = False
+
+    window.blit(blink[blinking_state], (0, 0))
 
     pygame.display.flip()
 
