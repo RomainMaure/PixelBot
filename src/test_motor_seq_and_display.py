0be4497140d92af55a987bd2683b6bd4c81511ce
maure <romain.maure@epfl.ch>
2022-07-18 23:14:12 +0200

    Test display + motor sequence

--- src/test_motor_seq_and_display.py
+++ src/test_motor_seq_and_display.py
@@ -1,6 +1,38 @@
-# System imports
+################################# IMPORTS #####################################
+
+import pygame
+from pygame.locals import *
+
 import time
 import RPi.GPIO as GPIO
+
+################################## FUNCTIONS ##################################
+
+def load_sequence(sequence_name, number_images, dim):
+    sequence = []
+
+    for i in range(number_images):
+        sequence.append(pygame.image.load("../img/" + sequence_name + "/" \
+            + sequence_name + str(i) + ".png").convert_alpha())
+
+    sequence = [pygame.transform.scale(img, dim) for img in sequence]
+
+    return sequence
+
+def play_sequence(emotion_sequence, count_emotion_state, emotion_state):
+    
+    count_emotion_state += 1
+
+    if count_emotion_state == CHANGE_STATE_TIME and \
+       emotion_state < len(emotion_sequence) - 1:
+
+        emotion_state += 1
+        count_emotion_state = 0
+
+        window.blit(emotion_sequence[emotion_state], (0, 0))
+
+    return count_emotion_state, emotion_state
+
 
 # TODO: autoformating scripts
 # TODO: StepPin -> step_pin_ ou _step_pin, define, etc
@@ -92,6 +124,27 @@ class SteppersHandler():
         self.motor_config_1.disable_sleep_mode()
         self.motor_config_2.disable_sleep_mode()
 
+################################## VARIABLES ##################################
+
+# Emotions
+CHANGE_STATE_TIME = 5
+
+count_emotion_state = 0
+emotion_state = 0
+
+# Eye blinking sequence
+BLINKING_TIME = 400
+BLINK_NUMBER_IMAGES = 5
+
+is_blinking = False
+count_blinking = 0
+
+# General use
+FPS = 100
+
+mainLoop = True
+
+
 # Define pins
 RESET_PIN_TILT_MOT = 6
 SLEEP_PIN_TILT_MOT = 13
@@ -108,33 +161,89 @@ pan_motor_config = StepperConfig(RESET_PIN_PAN_MOT, SLEEP_PIN_PAN_MOT, STEP_PIN_
 tilt_motor_config = StepperConfig(RESET_PIN_TILT_MOT, SLEEP_PIN_TILT_MOT, STEP_PIN_TILT_MOT, DIRECTION_PIN_TILT_MOT)
 steppersHandler = SteppersHandler(pan_motor_config, tilt_motor_config, 0.005)
 
-steppersHandler.disable_sleep()
+################################## MAIN #######################################
 
-steppersHandler.Simultaneous_step(50, pan_motor_config.ANTI_CLOCKWISE, tilt_motor_config.ANTI_CLOCKWISE)
+pygame.init()
 
-# Sleep 3 sec
+infoObject = pygame.display.Info()
+dimensions = (infoObject.current_w, infoObject.current_h)
+
+# 1920, 1080
+window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+pygame.mouse.set_visible(False)
+
+blink = load_sequence("blink", BLINK_NUMBER_IMAGES, dimensions)
+blink += reversed(blink)
+blink.remove(blink[len(blink)//2])
+
+window.blit(blink[0], (0, 0))
+
+clock = pygame.time.Clock()
+
+###
 start_time = time.time()
-steppersHandler.enable_sleep()
-while time.time() - start_time < 3.0:
-    time.sleep(0.1)
+motor_start_time = time.time()
 
-# Wake up
-steppersHandler.disable_sleep()
+motor_sequence_state_machine = 0
 
-steppersHandler.Step(70, pan_motor_config.CLOCKWISE, pan_motor_config)
+while mainLoop: 
 
-# Sleep 3 sec
-start_time = time.time()
-steppersHandler.enable_sleep()
-while time.time() - start_time < 3.0:
-    time.sleep(0.1)
+    clock.tick(FPS)
 
-# Wake up
-steppersHandler.disable_sleep()
-
-steppersHandler.Step(70, tilt_motor_config.CLOCKWISE, tilt_motor_config)
-
-steppersHandler.enable_sleep()
+    # Event catching
+    for event in pygame.event.get():
+        if event.type == KEYDOWN and event.key == K_ESCAPE:
+            mainLoop = False
 
 
+    # Emotions display 
+    count_blinking += 1
 
+    if count_blinking == BLINKING_TIME: # better with time ?
+        is_blinking = True
+
+    if is_blinking:
+        count_emotion_state, emotion_state = play_sequence(blink, 
+                                                    count_emotion_state,
+                                                    emotion_state)
+
+        if emotion_state == len(blink) - 1:
+            count_blinking = 0
+            emotion_state = 0
+            is_blinking = False
+
+
+    # Motor movement
+    if time.time() - motor_start_time > 5 and not is_blinking and motor_sequence_state_machine == 0:
+        steppersHandler.disable_sleep()
+
+        steppersHandler.Simultaneous_step(50, pan_motor_config.ANTI_CLOCKWISE, tilt_motor_config.ANTI_CLOCKWISE)
+
+        # Sleep 3 sec
+        motor_sequence_state_machine += 1
+        motor_start_time = time.time()
+        steppersHandler.enable_sleep()
+
+    if time.time() - motor_start_time > 5 and not is_blinking and motor_sequence_state_machine == 1:
+        steppersHandler.disable_sleep()
+
+        steppersHandler.Step(70, pan_motor_config.CLOCKWISE, pan_motor_config)
+
+        # Sleep 3 sec
+        motor_sequence_state_machine += 1
+        motor_start_time = time.time()
+        steppersHandler.enable_sleep()
+
+    if time.time() - motor_start_time > 5 and not is_blinking and motor_sequence_state_machine == 2:
+        steppersHandler.disable_sleep()
+
+        steppersHandler.Step(70, tilt_motor_config.CLOCKWISE, tilt_motor_config)
+
+        motor_sequence_state_machine += 1
+        steppersHandler.enable_sleep()
+        
+
+    pygame.display.flip()
+
+pygame.quit()
