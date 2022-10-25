64584452650f7183f01718d1d6b80b668b7ccf8e
romain <romain.maure@epfl.ch>
2022-10-25 10:16:13 +0200

    update tts test script

--- src/tts_test.py
+++ src/tts_test.py
@@ -1,8 +1,7 @@
-import win32api
 import pyttsx3
 
 engine = pyttsx3.init()
 engine.setProperty('volume',1.0) 
 
-engine.say("Yuan est un gros cochon")
+engine.say("This is a test")
 engine.runAndWait()
