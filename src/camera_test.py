4c8850f0a3b36093276b0326f9dc601dcd03b484
romain <romain.maure@epfl.ch>
2022-11-02 13:49:32 +0100

    Update camera test script, no use of PiCamera anymore as not available on bulleyes

--- src/camera_test.py
+++ src/camera_test.py
@@ -1,22 +1,15 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
 import cv2
 
-# Ubuntu -> cv2.videoCapture(0) ?
+# Initialize the camera
+cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.rotation = 180
-camera.resolution = (1280, 720) # 720p, (640, 480)
-rawCapture = PiRGBArray(camera)
+# Grab an image from the camera
+ret, frame = cap.read()
 
-# allow the camera to warmup
-time.sleep(2)
+# Save opencv image
+cv2.imwrite("image.jpg", frame)
 
-# grab an image from the camera
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
-
-# save opencv image
-cv2.imwrite("./image.jpg", image)
+# Release the camera
+cap.release()
