c04826f84201b4d41baa0fc000ab6e39001063ac
romain <romain.maure@epfl.ch>
2023-05-12 12:27:57 +0200

    update install.sh with normal ros2 install on ubuntu 22.04

--- src/install.sh
+++ src/install.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
 echo "===Installing ROS2 Humble==="
-curl -O https://raw.githubusercontent.com/RomainMaure/rpi-bullseye-ros2/main/install.bash
-bash install.bash humble aarch64 0.2.0 /opt/ros
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+sudo apt update
+sudo apt upgrade
+sudo apt install ros-humble-desktop
 
 echo "===Setting up a ROS2 workspace==="
 source /opt/ros/humble/setup.bash
@@ -23,11 +33,7 @@ sudo apt install python3-pip
 sudo apt install python3-pyaudio
 sudo apt install espeak
 sudo apt-get install flac
-sudo pip3 install opencv-python
 sudo pip3 install pyttsx3
-sudo pip3 install SpeechRecognition
 
-# Activate legacy camera ????
-# Test avec forked ros2 install -> should be ok now
 # tester si besoin de modifier file audio
 # chmod +x necessary ?
