0a6293561a2d2ce45a1115af250792041e2f00f2
romain <romain.maure@epfl.ch>
2023-05-31 16:49:06 +0200

    Add angle indication for motor testing script

--- src/test_scripts/motors_test.py
+++ src/test_scripts/motors_test.py
@@ -3,7 +3,7 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 while True:
-    angle = input("enter: ")
+    angle = input("enter an angle between 0 and 180: ")
     kit.servo[0].angle = int(angle)
     kit.servo[1].angle = int(angle)
     kit.servo[2].angle = int(angle)
